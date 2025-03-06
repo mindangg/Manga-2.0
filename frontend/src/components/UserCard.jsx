4f6258e1e3bb8ac53a741611dfdcbb4384f7202e
_.mindang <minhdang875425@gmail.com>
2025-03-06 09:15:54 +0700

    push

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -11,7 +11,7 @@ export default function UserCard() {
             <span>0901234567</span>
             <span>123 An Duong Vuong Ward 05</span>
             <span>19/05/2005</span>
-            <span className='user-status'>Available</span>
+            <span className='user-status'>Active</span>
             <span className='user-action'>
                 <i className='fa-solid fa-pen-to-square'></i>
                 <i className='fa-solid fa-trash-can'></i>
